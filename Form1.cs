f4bf5d2d7b7772fa972023d4e9fb4b2c29156a55
itresearch <itresearch010@gmail.com>
2022-02-13 22:23:36 +0300

    First commit

---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BackgroundQuotes
+{
+    public partial class Form1 : Form
+    {
+        private const int startTextSize = 100;
+        private const string fontName = "Segoe print";
+        private Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void selectImagesDir(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog(this);
+            tbImagePath.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void btnSelectQuotesFile(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog(this);
+            tbQuotesPath.Text = openFileDialog1.FileName;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IEnumerable<string> quotes = File.ReadLines(tbQuotesPath.Text);
+            DirectoryInfo dirInfo = new DirectoryInfo(tbImagePath.Text);
+            FileInfo[] imageFileInfos = dirInfo.GetFiles().Where(file => (file.Attributes & FileAttributes.Hidden) == 0)
+                .ToArray();
+            int index = 0;
+            foreach (string quote in quotes)
+            {
+                if (imageFileInfos.Length > index)
+                {
+                    CreateNewBackground(imageFileInfos[index++].Name, quote, rnd);
+                }
+            }
+        }
+
+        void CreateNewBackground(string fileName, string message, Random random)
+        {
+            Image image = Image.FromFile(Path.Combine(tbImagePath.Text, fileName));
+            Graphics graphics = Graphics.FromImage(image);
+            int x = 0;
+            int y = 0;
+            int width = image.Width;
+            int hight = Convert.ToInt32(image.Height * 0.25);
+            Font font = getFont(graphics, message, hight, width);
+
+            SolidBrush semiTransBrush = new SolidBrush(Color.FromArgb(128, 0, 0, 0));
+            graphics.FillRectangle(semiTransBrush, x, y, width, hight);
+            graphics.DrawString(message, font,
+                new SolidBrush(Color.FromArgb(random.Next(150, 255), random.Next(150, 255), random.Next(150, 255))),
+                new RectangleF(x, y, width, hight),
+                new StringFormat(StringFormatFlags.LineLimit));
+            image.Save(Path.Combine(tbOutputDir.Text, fileName), ImageFormat.Jpeg);
+        }
+
+
+        Font getFont(Graphics graphics, string message, int hight, int width)
+        {
+            Font font = new Font(fontName, startTextSize, FontStyle.Regular);
+            SizeF stringSize = graphics.MeasureString(message, font);
+            while (hight < stringSize.Height * (stringSize.Width / width + 1))
+            {
+                font = new Font(fontName, font.Size - 1.0F, FontStyle.Regular);
+                stringSize = graphics.MeasureString(message, font);
+            }
+
+            return font;
+        }
+
+        private void btnOutputDir(object sender, EventArgs e)
+        {
+            folderBrowserDialog2.ShowDialog(this);
+            tbOutputDir.Text = folderBrowserDialog2.SelectedPath;
+        }
+    }
+}
